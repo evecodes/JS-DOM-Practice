589db2cefbef63a274fe260245661b1142830c97
evecodes <99173892+evecodes@users.noreply.github.com>
2022-05-04 11:11:44 +0200

    Update 01
    Made two buttons work, updated some css.

--- JS_DOM_practice.js
+++ JS_DOM_practice.js
@@ -1,12 +1,31 @@
-// const body = document.body;
-
-// body.append("testest");
-
-// console.log("Hello world");
-
+// Button one actions
 const buttonOne = document.querySelector("#btn1");
 
 buttonOne.addEventListener("click", e => {
     buttonOne.classList.toggle('button-one');
     buttonOne.classList.toggle('button-one-alive');
 });
+
+
+// Button two actions
+
+
+
+// Button three actions
+
+
+
+// Button four actions
+const buttonFour = document.querySelector("#btn4");
+const hiddenMessage = document.querySelector("#hidden-message");
+
+buttonFour.addEventListener("click", e => {
+    buttonFour.classList.toggle('hide-button');
+    hiddenMessage.classList.toggle('hide-message');
+    timeout = setTimeout(retrieveBtn, 1000);
+});
+
+function retrieveBtn() {
+    buttonFour.classList.toggle('hide-button');
+    hiddenMessage.classList.toggle('hide-message');
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JS-DOM-Practice
  Javascript DOM practice
+
+ Update 01: Added functions to button one and four. Made preparations for button two. And some css cleanup.
